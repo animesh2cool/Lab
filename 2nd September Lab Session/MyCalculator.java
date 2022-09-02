60db3b0f6c7bb594b6e437d58f91cd4768287c5a
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-09-02 18:34:21 +0530

    Q1

--- 2nd September Lab Session/MyCalculator.java
+++ 2nd September Lab Session/MyCalculator.java
@@ -1,4 +1,4 @@
-/* 1. You are given a class AdvancedArithmetic which contains a method signature int divisor_sum(int n).
+/* Q1. You are given a class AdvancedArithmetic which contains a method signature int divisor_sum(int n).
 You need to write another class called MyCalculator which implements the method.
 divisorSum method just takes an integer as input and return the sum of all its divisors.
 For example divisors of 6 are 1, 2, 3 and 6, so divisor_sum should return 12. 
