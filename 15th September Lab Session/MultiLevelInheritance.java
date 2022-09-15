3ba411d665e7905a7127ae112101095160535dbd
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-09-15 17:54:07 +0530

    Q3

---
+++ 15th September Lab Session/MultiLevelInheritance.java
@@ -0,0 +1,22 @@
+/*Q3: Class A serves as a base class for the derived class B, which in turn serves as a base class for the derived class C. 
+(Which type Of Inheritance explain with an example)
+Ans: this a multi level inheritance
+ */
+
+class A {
+
+}
+
+class B extends A {
+
+}
+
+class C extends B {
+
+}
+
+public class MultiLevelInheritance {
+    public static void main(String args[]) {
+
+    }
+}
