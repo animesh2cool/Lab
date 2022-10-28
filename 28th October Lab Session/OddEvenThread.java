de3bef686c5dbe1e8f7676d60b108c584432eb9d
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-10-28 17:49:28 +0530

    problem 1

--- 28th October Lab Session/OddEvenThread.java
+++ 28th October Lab Session/OddEvenThread.java
@@ -1,18 +1,14 @@
 //Problem code 1. Create two threads to print even numbers and odd numbers from 1 to 100?
 //note: after printing of all even numbers only odd numbers should print
 
-
-
-import java.lang.Thread;;
-
 public class OddEvenThread {
     public static void main(String[] args) throws InterruptedException{
         Object lock = new Object(); 
-        Thread thread1 =  new Thread(new PrintOdd(lock));
-        Thread thread2 =  new Thread(new PrintEven(lock));
-        thread1.start();
-        thread1.join();
-        thread2.start();
+        Thread thread1 =  new Thread(new PrintOdd(lock));       //creating thread 1
+        Thread thread2 =  new Thread(new PrintEven(lock));      //creating thread 2
+        thread1.start();                //starting thread 1
+        thread1.join();                 //before start thread 2 join with thread 1
+        thread2.start();                //starting thread 2
         }
     }
 
@@ -22,7 +18,7 @@ class PrintEven implements Runnable{
         this.lock =  lock;
     }
     
-    public void run() {
+    public void run() {                             //overide
         synchronized (lock) {
             for (int i = 2; i <= 100; i+=2) {
                 System.out.println(i);
@@ -39,14 +35,12 @@ class PrintEven implements Runnable{
         this.lock =  lock;
     }
 
-    public void run() {
+    public void run() {                                //overide
         synchronized (lock) {
             for (int i = 1; i <= 100; i+=2) {
                 System.out.println(i);
                 lock.notify();
             }
         }
+      }
     }
-    }
-    
-    
