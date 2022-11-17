525c16eb81c1d19190375a0d1f168a87e281bb45
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-11-17 21:11:58 +0530

    P2

---
+++ 17th November Lab Session/ArrayListRetrieveEven.java
@@ -0,0 +1,26 @@
+//Problem Statement 2:
+import java.util.ArrayList;
+
+public class ArrayListRetrieveEven {
+	private ArrayList<Integer> list = new ArrayList<Integer>();
+	public ArrayList<Integer> storeEvenNumbers(int N) {
+		list = new ArrayList<Integer>();
+		for (int i = 2; i <= N; i++) {
+			if (i % 2 == 0) list.add(i);
+		}	
+		return list;
+	}
+	public ArrayList<Integer> printEvenNumbers() {
+		ArrayList<Integer> newList = new ArrayList<Integer>();
+		for (int item : list) {
+			newList.add(item * 2);
+			System.out.println(item * 2);
+		}
+		return newList;
+	}
+	public static void main(String[] args) {
+		ArrayListRetrieveEven asg = new ArrayListRetrieveEven();
+		asg.storeEvenNumbers(10);
+		asg.printEvenNumbers();
+	}
+}
