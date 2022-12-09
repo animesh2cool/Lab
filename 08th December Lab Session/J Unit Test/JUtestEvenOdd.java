9bc3dd3acd2395b47c2695d0df36a5fce826e7ed
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-12-09 13:13:27 +0530

    Odd Even Method

---
+++ 08th December Lab Session/J Unit Test/JUtestEvenOdd.java
@@ -0,0 +1,10 @@
+//method to test even and odd numbers
+
+public class JUtestEvenOdd {
+    static boolean testEvenOdd(int a) {
+        if (a % 2 == 0) {
+            return true;                    //return true if parameter is devided by two or a even number
+        }
+        return false;                       //return false if parameter is a odd number
+    }
+}
